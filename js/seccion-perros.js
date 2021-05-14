63a8cbf10033732ccffac4269c43f52ce94a8930
Osvaldo Rodriguez <osvaldorodriguez.globant@latam.com>
2021-05-13 20:34:32 -0400

    Se agrega mejora al ocultar el spiner cuando se completa el listado de gatos

--- js/seccion-perros.js
+++ js/seccion-perros.js
@@ -1,28 +1,3 @@
-function actualizarImagen(identificador) {
-
-    console.log("mostrar spiner para reflejar la ejecución de la llamada ajax de fondo");
-    $("#ajax-loading-"+identificador).removeClass("invisible");
-
-    $.get("https://api.thedogapi.com/v1/images/search?limit=1", function(response){
-        
-        //iterar por el único elemento devuelto por la lista
-        $.each(response, function(index, element){
-            //crear imagen para precargar antes de reemplazar por la existente
-            var newImagen = new Image();
-            newImagen.src = element.url;
-
-            //cuando la imagen ya se encuentra descargada
-            newImagen.onload = function() {
-
-                // se reemplaza la seleccionada por la descargada
-                $("#imagen-"+identificador).attr("src",element.url);
-                
-                console.log("ocultando spiner");
-                $("#ajax-loading-"+identificador).addClass("invisible");
-            };               
-        });
-    });
-}
 
 $(document).ready(function() {
 
@@ -34,12 +9,7 @@ $(document).ready(function() {
             registroHTML += "  </td>"
             registroHTML += "  <td>"
             registroHTML += "      <img id=\"imagen-"+element.id+"\"  src=\""+element.url+"\" height=\"100\" width=\"auto\" />"
-            registroHTML += "  </td>"
-            registroHTML += "  <td>"
-            registroHTML += "      "+ ((!$.isArray(element.breeds) || !element.breeds.length)? "Sin Información" :element.breeds[0].name)
-            registroHTML += "  </td>"
-            registroHTML += "  <td>"
-            registroHTML += "      "+ ((!$.isArray(element.breeds) || !element.breeds.length)? "Sin Información" :element.breeds[0].temperament)
+            registroHTML += "      <div id=\"ajax-loader-"+element.id+"\" class=\"spinner-border text-info\"></div>"
             registroHTML += "  </td>"
             registroHTML += "  <td>"
             registroHTML += "      <button class=\"btn btn-info\" onclick=\"javascript:actualizarImagen('"+element.id+"');\"  >"
@@ -51,4 +21,29 @@ $(document).ready(function() {
         });
     })
 
+    function actualizarImagen(identificador) {
+
+        console.log("mostrar spiner para reflejar la ejecución de la llamada ajax de fondo");
+        $("#ajax-loading-"+identificador).removeClass("invisible");
+    
+        $.get("https://api.thedogapi.com/v1/images/search?limit=1", function(response){
+            
+            //iterar por el único elemento devuelto por la lista
+            $.each(response, function(index, element){
+                //crear imagen para precargar antes de reemplazar por la existente
+                var newImagen = new Image();
+                newImagen.src = element.url;
+    
+                //cuando la imagen ya se encuentra descargada
+                newImagen.onload = function() {
+    
+                    // se reemplaza la seleccionada por la descargada
+                    $("#imagen-"+identificador).attr("src",element.url);
+                    
+                    console.log("ocultando spiner");
+                    $("#ajax-loading-"+identificador).addClass("invisible");
+                };               
+            });
+        });
+    }
 });
